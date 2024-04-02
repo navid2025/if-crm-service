b07a21a0d51e66ff5f0d8ea35ddfdb737d747ed1
n.zare <n.zare@behsacorp.com>
2024-04-02 17:02:13 +0330

    initial commit

---
+++ src/main/java/com/behsa/crm/IfCrmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.behsa.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfCrmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfCrmServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/behsa/crm/IfCrmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behsa.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfCrmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
